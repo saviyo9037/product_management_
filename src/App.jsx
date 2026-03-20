4435fb3ec41f46345d2994cf3e2a0fed902ea0b2
saviyo george <saviyogeorge903734@gmail.com>
2026-03-20 19:59:50 +0530

    app.jsx and services align fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Sidebar from './components/Sidebar'
-import Header from './components/Header'
-import ProductList from './pages/ProductList'
-import AddProduct from './pages/AddProduct'
-import EditProduct from './pages/EditProduct'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import ProductList from "./pages/ProductList";
+import AddProduct from "./pages/AddProduct";
+import EditProduct from "./pages/EditProduct";
 
-import ProductF from './components/ProductF'
+import ProductF from "./components/ProductF";
 
 function App() {
   return (
-   <BrowserRouter>
-   <div className='flex bg-gray-100'>
-    
-    <Sidebar/>
-    <div className=' flex-1'>
-    <Header/>
+    <BrowserRouter>
+      <div className="flex bg-gray-100">
+        <Sidebar />
+        <div className=" flex-1">
+          <Header />
 
-    <Routes>
-      <Route  path='/'  element={<ProductList/>}/>
-      <Route  path='/add'  element={<AddProduct/>}/>
-<Route  path='/edit/:id'  element={<EditProduct/>}/>
-
-
-
-
-    </Routes>
-    </div>
-
-    </div></BrowserRouter>
-  )
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/add" element={<AddProduct />} />
+            <Route path="/edit/:id" element={<EditProduct />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
